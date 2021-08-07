7c15086e4930456114f91cee25b2d1dbc11a6dfa
sungwony0906 <sungwony0906@gmail.com>
2021-08-07 23:54:37 +0900

    10. API 개발 고급 - 지연 로딩과 성능 최적화
    - 간단한 주문 조회 V1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.api;
+
+import java.util.List;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        return orderRepository.findAll(new OrderSearch());
+    }
+}
